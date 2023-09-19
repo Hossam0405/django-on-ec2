cb694538c36daf6b049adf6fdf642fb8f66f6a0b
Hossam eldin Rashad <87908524+Hossam0405@users.noreply.github.com>
2023-09-19 23:30:27 +0300

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
